e866273003cbc572d37bdaf4f80c3708f8c302e4
AmruthaR-04 <149512904+AmruthaR-04@users.noreply.github.com>
2024-03-03 13:09:12 +0530

    Add files via upload

---
+++ settings/Test Suites/Amazon_TestSuite/Amazon_TestSuite_Search_An_Item.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amazon_TestSuite_Search_An_Item</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5d86dd90-cedc-4820-bafc-74bd7fafb6c2</testSuiteGuid>
+   <testCaseLink>
+      <guid>ae351d7f-c7c8-4e6b-aa65-554d645ed299</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Amazon/TC_Amazon_Search_An_Item</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
