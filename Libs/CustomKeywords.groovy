acd28257678b43e1b94208c2c6a4bee3b143f0c8
AmruthaR-04 <149512904+AmruthaR-04@users.noreply.github.com>
2024-03-04 23:43:56 +0530

    Add files via upload

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,18 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+
+
+def static "com.test.demo.Amazonpage.CheckDropElementKatalon"(
+    	TestObject object	
+     , 	String option	) {
+    (new com.test.demo.Amazonpage()).CheckDropElementKatalon(
+        	object
+         , 	option)
+}
